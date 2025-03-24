6b60a1d702688848e14f322846831ce5e05007a0
axelbol <axel_lp_alvarado@hotmail.com>
2025-03-24 00:30:49 -0400

    search input & filter categories

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,28 @@
 <x-main-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged index!") }}
-                </div>
+    <div class="filters flex space-x-6">
+        <div class="w-1/3">
+            <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Category One">Category One</option>
+                <option value="Category Two">Category Two</option>
+                <option value="Category Three">Category Three</option>
+                <option value="Category Four">Category Four</option>
+            </select>
+        </div>
+        <div class="w-1/3">
+            <select name="other_filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Filter One">Filter One</option>
+                <option value="Filter Two">Filter Two</option>
+                <option value="Filter Three">Filter Three</option>
+                <option value="Filter Four">Filter Four</option>
+            </select>
+        </div>
+        <div class="w-2/3 relative">
+            <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8">
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
             </div>
         </div>
-    </div>
+    </div> <!-- end filters -->
 </x-main-layout>
